c555d4e610895b3a8c8cb24d734564ca0712e17a
Ardavan <a.sassani@gmail.com>
2019-07-01 16:05:12 -0400

    Add minmaxscale class

---
+++ src/utilities/functions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from scipy.signal import argrelextrema
+
+class MinMaxScale():
+    def __init__(self, df:pd.DataFrame):
+        self.mins = df.min()
+        self.maxs = df.max()
+        self.delta = maxs - mins
+
+    def normalize(self, df: pd.DataFrame):
+        return (df - self.mins)/self.delta
+
+    def denormalize(self, df: pd.DataFrame):
+        return df * self.delta + self.mins
